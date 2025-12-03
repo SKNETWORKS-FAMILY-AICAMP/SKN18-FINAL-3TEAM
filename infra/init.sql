af8c2fc8cc263a28f1a976f8a12c2b792386f500
seyung000 <seyung.park000@gmail.com>
2025-12-03 11:27:13 +0900

    temp:dev 업데이트를 위한 임시저장

---
+++ infra/init.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- 예제 테이블 생성
+CREATE TABLE documents (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(20),
+    title VARCHAR(255),
+    summary TEXT,
+    content TEXT,                 -- 문서 내용
+    embedding VECTOR(1536),       -- 임베딩 크기에 맞춤
+    metadata JSONB                -- 메타데이터
+);
