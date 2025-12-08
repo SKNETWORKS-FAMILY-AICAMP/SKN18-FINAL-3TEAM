bfc3676270684e5a2410a031e570b8c9fe27a62f
ansi212972-only <ansi212972@gmail.com>
2025-12-08 19:45:05 +0900

    refector : mypage app 제거

--- backend/django/mypage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-# MyPage app URLs
-# React frontend handles the mypage UI
-# Add API endpoints here for user profile functionality
-
-urlpatterns = [
-    # API endpoints will be added here
-]
