e77f567cebc4cd3decbe3df8313ec7dd9fd941fd
poolbbang <igeun2002@hanmail.net>
2025-12-12 17:32:37 +0900

    refactor: 테스트 용으로, 지금 못 찾는 API 경로를 잠시 주석처리해서 에러 방지

--- backend/django/config/urls.py
+++ backend/django/config/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     # 인증/계정 API
-    path('api/', include('accounts.urls')),
+    # path('api/', include('accounts.urls')),
 
     # 사용자 관리 API
-    path('api/users/', include('users.urls')),
+    # path('api/users/', include('users.urls')),
 
     # 검색 API
-    path('api/search/', include('search.urls')),
+    # path('api/search/', include('search.urls')),
 
     # 영상/LLM 관련 API
     path('api/video/', include('video.urls')),
