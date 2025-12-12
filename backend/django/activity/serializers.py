2142a13f7303926f7cc00ee28de758b1380d1e60
ansi212972-only <ansi212972@gmail.com>
2025-12-12 11:16:54 +0900

    feat : django activity 생성

---
+++ backend/django/activity/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from .models import WatchingHistory, SearchHistory
+
+
+class WatchingHistorySerializer(serializers.ModelSerializer):
+    """시청 기록 조회용 serializer"""
+    user_email = serializers.EmailField(source='user.email', read_only=True)
+    user_nickname = serializers.CharField(source='user.nickname', read_only=True)
+    video_title = serializers.CharField(source='video.title', read_only=True)
+
+    class Meta:
+        model = WatchingHistory
+        fields = [
+            'id',
+            'video',
+            'video_title',
+            'user',
+            'user_email',
+            'user_nickname',
+            'tags',
+            'watched_seconds',
+            'created_at',
+        ]
+        read_only_fields = ['id', 'created_at', 'user']
+
+
+class WatchingHistoryCreateSerializer(serializers.ModelSerializer):
+    """시청 기록 적재용 serializer"""
+
+    class Meta:
+        model = WatchingHistory
+        fields = ['video', 'tags', 'watched_seconds']
+
+    def create(self, validated_data):
+        # user는 request.user에서 자동으로 설정
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class SearchHistorySerializer(serializers.ModelSerializer):
+    """검색 기록 조회용 serializer"""
+    user_email = serializers.EmailField(source='user.email', read_only=True)
+    user_nickname = serializers.CharField(source='user.nickname', read_only=True)
+
+    class Meta:
+        model = SearchHistory
+        fields = [
+            'id',
+            'user',
+            'user_email',
+            'user_nickname',
+            'search_query',
+            'created_at',
+        ]
+        read_only_fields = ['id', 'created_at', 'user']
+
+
+class SearchHistoryCreateSerializer(serializers.ModelSerializer):
+    """검색 기록 적재용 serializer"""
+
+    class Meta:
+        model = SearchHistory
+        fields = ['search_query']
+
+    def create(self, validated_data):
+        # user는 request.user에서 자동으로 설정
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
