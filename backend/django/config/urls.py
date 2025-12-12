af9e22e8b5b1a73419951b67a3ab607a0c01b349
ansi212972-only <ansi212972@gmail.com>
2025-12-12 17:30:04 +0900

    fix : view 나눠진거 하나로 합치기

--- backend/django/config/urls.py
+++ backend/django/config/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
 ]
 
 # 개발 환경에서 media 파일 서빙
