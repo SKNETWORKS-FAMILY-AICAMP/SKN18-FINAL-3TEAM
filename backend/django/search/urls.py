f672a38fb1a9869eb994f18f2afb358ff63ab687
ansi212972-only <ansi212972@gmail.com>
2025-12-12 11:14:03 +0900

    del : django search앱 제거

--- backend/django/search/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-# Search app URLs
-# React frontend handles the search page UI
-# Add API endpoints here for search functionality
-
-urlpatterns = [
-    # API endpoints will be added here
-]
